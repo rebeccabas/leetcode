5be5d69b95cac8a9483f2f63abf213a63be6f623
Rubika Bashyal <103018188+rebeccabas@users.noreply.github.com>
2025-09-11 19:09:25 +0545

    Time: 18 ms (7.99%) | Memory: 8.5 MB (84.78%) - LeetSync

---
+++ 69-sqrtx/sqrtx.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        long i =0;
+        while((i*i)<x){
+            i++;
+        }
+        if(i*i>x){
+            return i-1;
+        }
+            return i;
+    }
+};
